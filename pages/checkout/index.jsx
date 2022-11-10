f06c48a2a71692bdc0bb3075423e28dca34f34bc
luissena <luis.senarode@gmail.com>
2022-11-10 13:54:14 -0300

    finished

---
+++ pages/checkout/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Checkout() {
+  return (
+    <div className='flex'>
+        <h1 className='m-auto text-8xl'>Checkout</h1>
+    </div>
+  )
+}
+
+export default Checkout
---
+++ pages/produto/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductFeatures from '../../components/organisms/Marketplace/ProductFeatures'
+import ProductsOverview from '../../components/organisms/Marketplace/ProductsOverview'
+
+export default function Produto() {
+  return (
+<>
+    <ProductsOverview />
+    <ProductFeatures/>
+  </>
+  )
+}
